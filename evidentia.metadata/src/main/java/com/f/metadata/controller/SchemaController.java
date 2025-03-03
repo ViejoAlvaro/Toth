21a7b3741443db0c8dba49e78bacdcde8ad364aa
Alvaro Lopez <lopeztothsys@gmail.com>
2025-03-02 19:06:42 -0500

    Added @RequestMappings to the Schema Controller

--- evidentia.metadata/src/main/java/com/f/metadata/controller/SchemaController.java
+++ evidentia.metadata/src/main/java/com/f/metadata/controller/SchemaController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.f.metadata.model.Schema;
@@ -29,10 +32,15 @@ public class SchemaController {
         return ResponseEntity.ok(schema);
     }
 
-    @GetMapping("/{name}")
-    public ResponseEntity<Schema> getSchemaByName(@PathVariable String name) {
+    @GetMapping("/getSchema")    
+    public ResponseEntity<Schema> getSchemaByName(@RequestParam  String name) {
         Schema schema = schemaService.getSchemaByName(name);
         return ResponseEntity.ok(schema);
+    }
+    
+    @RequestMapping(value = "/addSchema", method = RequestMethod.POST)
+    public ResponseEntity<Schema> addSchema(@RequestBody Schema schema){
+    	return ResponseEntity.ok(schema);
     }
 
 }
